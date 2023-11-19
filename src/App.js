ba564a0e79460ea5edb4ad76476eb3ee1e13e1b0
Thuyatun <thuratundev@gmail.com>
2023-11-19 23:39:38 +0630

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [count, SetCount] = useState(10);
+  const [Year, setYear] = useState('Happy New Year');
+  useEffect(() => {
+    if (count > 0) {
+      const interval = setInterval(() => {
+        SetCount(count - 1)
+      }, 1000)//1 second
+
+      if (count === 1) {
+        setYear(Year)
+      }
+
+      return () => {
+        clearInterval(interval)
+      }
+    }
+  }, [count, Year])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {count > 1 ? <h1>
+        {count}
+      </h1> :
+        <h1 className='year'>{Year}</h1>
+      }
+    </div >
   );
 }
 
